e8708321fa9708c3dc75b9359579bc5130058417
Matteo Juen <matteo.juen@ematric.com>
2024-03-26 12:51:40 +0100

    fix: hostname reader

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "blvckleg.github.io",
+        pathname: '/portfolio/**',
       },
     ],
   },
