776b9292ecd7fff025ad4443f4bd5ef1e4bbf6b8
Matteo Juen <matteo.juen@ematric.com>
2024-03-26 13:56:40 +0100

    feat: export static html

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "blvckleg.github.io",
-        pathname: '/portfolio/**',
+        
+        pathname: '/portfolio',
       },
     ],
   },
