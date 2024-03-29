03dbd327e6582d5c73bf6f607fbc697adf2a2b1f
matteom0165 <matteo.juen@ematric.com>
2024-03-29 17:21:37 +0100

    style: typo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -69,7 +69,7 @@ export function Navbar() {
     <MTNavbar shadow={false} fullWidth className="border-0 sticky top-0 z-50"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
       <div className="container mx-auto flex items-center justify-between">
         <Typography color="blue-gray" className="text-lg font-bold"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          Blvckleg - Full-stack developer
+          Blvckleg | Full-stack developer
         </Typography>
         <ul className="ml-10 hidden items-center gap-8 lg:flex">
           {NAV_MENU.map(({ name, icon: Icon, href }) => (
